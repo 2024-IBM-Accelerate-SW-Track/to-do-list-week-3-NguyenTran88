64f225dc0f58e7cd960e671e5110bece6a4ad502
NguyenTran88 <nqt2001@columbia.edu>
2024-06-24 02:02:37 -0400

    Done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,7 +29,12 @@ class Home extends Component {
   // updates the state with the new list.
   addTodo = (todo) => {
     const exists = this.state.todos.find(t => t.content === todo.content);
-    if (exists){ return }
+    if (exists) { return }
+    let tempDue = todo.due;
+    if (tempDue === null || tempDue === "Invalid Date") {
+      console.log(tempDue, 1);
+      return;
+    }
     // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
     // should not share duplicate values.
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
